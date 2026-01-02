7ceb1f6827309ab07119aa9bf9968ba6fb69028b
Edgar Mendes <mendesbr92@gmail.com>
2026-01-02 20:09:08 -0300

    test: criado os testes para marcar tarefa, listar, filtrar

--- src/test/java/com/br/service/TaskServiceImplTest.java
+++ src/test/java/com/br/service/TaskServiceImplTest.java
@@ -22,6 +22,7 @@ public class TaskServiceImplTest {
     void setUp() {
 
         repository = new TaskRepositoryImpl();
+        Task.resetar();
         task = new Task("oi", Prioridade.ALTA, "Java", Status.PENDENTE, "");
         service = new TaskServiceImpl(repository);
 
@@ -57,4 +58,74 @@ public class TaskServiceImplTest {
 
     }
 
+    @Test
+    void listarTarefaPorIdDeveLancarException() {
+
+        IllegalArgumentException e =
+                assertThrows(IllegalArgumentException.class, () -> service.listarTarefaPorID(-1));
+
+        assertTrue(e.getMessage().equals("O ID da tarefa deve ser maior que zero"));
+    }
+
+    @Test
+    void marcarTarefaComoConcluidaDeveLancarException() {
+
+        TaskNullResponse e = assertThrows(TaskNullResponse.class, () -> service
+                .marcarTarefaComoConcluida((int) new Task(null, null, null, null, null).getId()));
+
+        assertTrue(e.getMessage().equals("Não e permitido valores nullos"));
+    }
+
+    @Test
+    void marcarTarefaComoConcluidaComSucesso() {
+
+        repository.adicionarTarefa(task);
+
+        service.marcarTarefaComoConcluida((int) task.getId());
+
+        assertEquals(Status.CONCLUIDO, task.getStatus());
+    }
+
+    @Test
+    void listarTarefasConcluidasComSucesso() {
+
+        repository.adicionarTarefa(task);
+
+        service.marcarTarefaComoConcluida((int) task.getId());
+
+        assertEquals(1, repository.listarTarefasConcluidas().size());
+    }
+
+    @Test
+    void filtrarTarefasPorPrioridadeComSucesso() {
+
+        Task task1 = new Task("tarefa1", Prioridade.ALTA, "Java", Status.PENDENTE, "");
+        Task task2 = new Task("tarefa2", Prioridade.ALTA, "Java", Status.PENDENTE, "");
+        Task task3 = new Task("tarefa3", Prioridade.ALTA, "Java", Status.PENDENTE, "");
+        Task task4 = new Task("tarefa4", Prioridade.BAIXA, "Java", Status.PENDENTE, "");
+
+        repository.adicionarTarefa(task1);
+        repository.adicionarTarefa(task2);
+        repository.adicionarTarefa(task3);
+        repository.adicionarTarefa(task4);
+
+        assertEquals(3, service.filtrarTarefasPorPrioridade().size());
+
+    }
+
+    @Test
+    void listarTarefasPendentesComSucesso() {
+
+        Task task1 = new Task("tarefa1", Prioridade.ALTA, "Java", Status.PENDENTE, "");
+        Task task2 = new Task("tarefa2", Prioridade.ALTA, "Java", Status.PENDENTE, "");
+        Task task3 = new Task("tarefa3", Prioridade.ALTA, "Java", Status.PENDENTE, "");
+        Task task4 = new Task("tarefa4", Prioridade.BAIXA, "Java", Status.CONCLUIDO, "");
+
+        repository.adicionarTarefa(task1);
+        repository.adicionarTarefa(task2);
+        repository.adicionarTarefa(task3);
+        repository.adicionarTarefa(task4);
+
+        assertEquals(3, service.listarTarefasPendentes().size());
+    }
 }
