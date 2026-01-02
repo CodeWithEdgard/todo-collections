9b6b306aca32fa9c00655a9bfe42b9e0272f4d97
Edgar Mendes <mendesbr92@gmail.com>
2026-01-02 01:25:17 -0300

    docs: alterado a msg do app

--- src/main/java/com/br/application/Application.java
+++ src/main/java/com/br/application/Application.java
@@ -1,33 +1,11 @@
 package com.br.application;
 
-
-import com.br.domain.Task;
-import com.br.domain.enums.Prioridade;
-import com.br.domain.enums.Status;
-import com.br.repository.TaskRepository;
-import com.br.repository.TaskRepositoryImpl;
-import com.br.service.TaskService;
-import com.br.service.TaskServiceImpl;
-
 public class Application {
 
         public static void main(String[] args) {
 
-
-                Task tarefa = new Task("Collections em Java", Prioridade.ALTA, "Java",
-                                Status.EM_ANDAMENTO); // modelo
-
-                Task tarefa1 = new Task("Orientação a objetos", Prioridade.MEDIA, "Java",
-                                Status.CONCLUIDO); // modelo
-
-                TaskRepository repository = new TaskRepositoryImpl(); // dados
-
-                TaskService service = new TaskServiceImpl(repository);
-
-                service.adicionarTarefa(tarefa);
-                service.adicionarTarefa(tarefa1);
-
-                System.out.println(service.listarTodasTarefas());
+                System.out.println("Bem Vindo ao To Do Collections");
+                System.out.println("Criado para Desenvolver minha Lógica e uso das Collecions");
 
         }
 }
