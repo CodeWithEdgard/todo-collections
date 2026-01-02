73532a323149e5d84bae027cd5fb6f73a98680ec
Edgar Mendes <mendesbr92@gmail.com>
2026-01-02 20:01:54 -0300

    fix: corrigido para adicionar nivel de prioridade e metodo de ordem

--- src/main/java/com/br/domain/enums/Prioridade.java
+++ src/main/java/com/br/domain/enums/Prioridade.java
@@ -1,5 +1,15 @@
 package com.br.domain.enums;
 
 public enum Prioridade {
-    ALTA, MEDIA, BAIXA
+    ALTA(1), MEDIA(2), BAIXA(3);
+
+    private final int ordem;
+
+    Prioridade(int ordem) {
+        this.ordem = ordem;
+    }
+
+    public int getOrdem() {
+        return ordem;
+    }
 }
